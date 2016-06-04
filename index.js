2d9c4e2e80e0d748d72832374dd517b3ce15cf50
jaywcjlove <398188662@qq.com>
2016-06-04 19:47:47 +0800

    Understanding offline caching,code refactoring.
    1. param adjustment.
    2. code adjustment.

--- index.js
+++ index.js
@@ -4,64 +4,79 @@ var path = require('path');
 
 var ManifestGenerator = function(options){
     this.options = _.extend({
-      filename: 'app.manifest',
-      network:[],
-      fallback:{},
-      ext:'*',
-      isWatch: false,
-      htmlpath:[]
+        dest:'cache.manifest',
+        headcomment: "",
+        cache:[],
+        network:[],
+        fallback:[],
+        timestamp: true, // 显示时间
+        isWatch: false,
+        master:[]
     }, options);
 }
-
-function inNETWORK(file,arr){
-    var isnt = false
-    for (var i = 0; i < arr.length; i++) {
-        if(file.indexOf(arr[i])>-1){
-            isnt = true;
-            break;
-        }
-    }
-    return isnt;
-}
-
 ManifestGenerator.prototype.apply = function(compiler){
     var self = this;
-    this.options = _.extend(this.options, {isWatch: compiler.options.watch || false})
-
-    // 序列出 HTML 文件路径及名字
-    compiler.plugin('compilation', function(compilation, params) {
-        compilation.plugin('after-optimize-chunk-assets', function(chunks) {
-            chunks.map(function(c){
-                if( c.files.length===1 && /(.html|.htm)+(\?|\#|$)/.test(c.files[0]) ){
-                    self.options.htmlpath.push(c.files[0])
-                }
-            })
-        });
-    });
-
+    this.options = _.extend(this.options, {isWatch: compiler.options.watch || false});
     // 编译生成
     compiler.plugin('emit', function(compilation, callback) {
         self.setHTMLManifest(compilation)
         self.creatManifest(compilation);
         callback();
     });
-
 }
-
+/**
+ * 给页面添加 manifest入口文件引用
+ * @param {[type]} compilation 
+ */
 ManifestGenerator.prototype.setHTMLManifest = function(compilation){
     var self = this;
-    self.options.htmlpath.map(function(_path){
+    var dest = this.options.dest;
+    this.options.master.forEach(function(_path){
         var data = compilation.assets[_path];
-        if (!data) return;
         var source_str = data.source();
         compilation.assets[_path] = {
             source: function() {
-                return source_str.replace('<html','<html manifest="'+self.options.filename+'"');
+                var url = path.relative(_path,dest)
+                return source_str.replace(/<html[^>]*manifest="([^"]*)"[^>]*>/,function(word){
+                   return word.replace(/manifest="([^"]*)"/,'manifest="'+url+'"');
+                }).replace(/<html(\s?[^\>]*\>)/,function(word){
+                    if(word.indexOf('manifest')) return word;
+                    return word.replace('<html','<html manifest="'+url+'"')
+                });
             },
             size: function() {
                 return source_str.length;
             }
         };
+    });
+}
+/**
+ * [displayPath 返回真实URL]
+ * @param  {[type]} compilation [description]
+ * @param  {[type]} _path       [带hash 的 URL=> 'js/[hash:8].sorting-index.js']
+ * @return {[type]}             [string]
+ */
+ManifestGenerator.prototype.displayPath = function(compilation,_path){
+    var dtarr = compilation.getStats().toJson();
+    var url_regx = _path.replace(/\[(?:(\w+):)?(contenthash|hash)(?::([a-z]+\d*))?(?::(\d+))?\]/ig,'(.*)')
+    var reg = new RegExp(url_regx);
+    return dtarr.assets.map(function(item,idx){
+         if(reg.test(item.name)){
+             return item.name;
+         }else{
+             return ''
+         }
+     }).join('');
+}
+ManifestGenerator.prototype.urlPathTo = function(compilation,arr){
+    var self = this;
+    return arr.map(function(item){
+        if(/(.css|.jpg|.jpeg|.gif|.svg|.png)+(\?|\#|$)/.test(item)){
+            item = self.displayPath(compilation,item)
+        }else{
+            item = compilation.getPath(item)
+        }
+        return item
     })
 }
 /**
@@ -69,54 +84,46 @@ ManifestGenerator.prototype.setHTMLManifest = function(compilation){
  * @param  {[type]} compilation [The Compilation instance extends from the compiler.]
  */
 ManifestGenerator.prototype.creatManifest = function(compilation){
-    var manifest = 'CACHE MANIFEST\n# Time：' + new Date() + '\n';
-    var fallback = this.options.fallback;
-    var network = this.options.network;
-    var filename = this.options.filename;
-    var ext = this.options.ext;
+
+    var contents = [],
+        options = this.options,
+        fallback = options.fallback,
+        network = options.network,
+        dest = options.dest;
     var outputData = [];
     var networkData = [];
-    _.forOwn(compilation.assets, function(value, url) {
-        if(!inNETWORK(url,network)){
-            if(ext === "*") outputData.push(url);
-            else{
-                var strRegex = "("+ext+")";
-                var re=new RegExp(strRegex);
-                if (re.test(str)){
-                    outputData.push(url);
-                }
-            }
 
-        }
-        if(inNETWORK(url,network)){
-            networkData.push(url)
-        }
-    });
-
-    if(compilation.records) for (var url in compilation.records) {
-        if(url.indexOf('html-webpack-plugin')>-1){
-            var urld = url.match(/html-webpack-plugin for \"(.*)\"/);
-            if(RegExp.$1 && !inNETWORK(RegExp.$1,network)){
-                outputData.push(RegExp.$1);
-            }else{
-                (RegExp.$1)&&networkData.push(RegExp.$1);
-            }
-        }
+    contents.push('CACHE MANIFEST');
+    if (options.timestamp) {
+        contents.push('# Time: ' + new Date());
+    }
+    if (options.headcomment) {
+        contents.push('# '+options.headcomment);
+    }
+    contents.push('');
+    if(options.cache){
+        contents.push('CACHE:');
+        contents = contents.concat(this.urlPathTo(compilation,options.cache));
+        contents.push('');    
     }
 
-    manifest += outputData.join('\n');
+    if(network.length>0){
+        contents.push('NETWORK:');
+        contents.push(network.join('\n'));
+        contents.push('');    
+    }
 
-    if(networkData.length>0){
-        manifest += '\n\nNETWORK:\n';
-        manifest += networkData.join('\n');
+    if(fallback.length>0){
+        contents.push('FALLBACK:');
+        contents.push(fallback.join('\n'));
     }
     // Insert this list into the Webpack build as a new file asset:
-    compilation.assets[filename] = {
+    compilation.assets[dest] = {
         source: function() {
-          return manifest;
+          return contents.join('\n');
         },
         size: function() {
-          return manifest.length;
+          return contents.join('\n').length;
         }
     };
 
